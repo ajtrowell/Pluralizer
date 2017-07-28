e3239ef7a0c4aac7a1c2bbf0efa7074c2a1fb972
AJTrowell <ajtrowell@gmail.com>
2017-07-28 15:17:24 -0500

    Initial Pluralizer javascript solution.
    Demonstrating handling irregular plural forms.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Pluralizer
+// Takes two arguments, a number and a singular noun
+// Return plural or singular noun, eg '1 dog' or '2 cats' as string.
+// Handles limited numbers of irregularNouns, like sheep or geese.
+//
+
+
+
+function pluralizer(num,noun) {
+    var resultString;
+
+
+
+    return resultString;
+}
