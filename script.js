79e44a0e6113ed251de75c517a48800b2c1d6fdf
AJTrowell <ajtrowell@gmail.com>
2017-07-28 15:28:37 -0500

    Implemented basic functionality.
    Doesn't yet handle irregular noun plurals.

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ function pluralizer(num,noun) {
     var resultString;
 
 
+    // Handle normal nouns
+    if(num === 1) {
+        resultString = num + " " + noun;
+    } else if (num > 1) {
+        resultString = num + " " + noun + "s";
+    } else {
+        show('Error: Invalid number!');
+    }
+
 
     return resultString;
 }
+
+
+function show(arg) {
+    document.write(arg + "<br>");
+}
+
+
+
+// Call function:
+show(pluralizer(1,'dog'));
+show(pluralizer(5,'cat'));
+show(pluralizer(4,'sheep'));
+show(pluralizer(5,'goose'));
+
