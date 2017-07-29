b2b7b53c97488ff42e85f773a1f1f22c1a7dd7fd
AJTrowell <ajtrowell@gmail.com>
2017-07-28 23:15:06 -0500

    Added test function with inputs and expected outputs.
    testPluralizer() checks its internal list of input/output pairs.
    If the actual result differs from the expected output, the
    expected result is output in red letters.
    
    Could add the remaining examples to the test list for completeness.

--- script.js
+++ script.js
@@ -58,6 +58,10 @@ function show(arg) {
     document.write(arg + "<br>");
 }
 
+function showError(arg) {
+    document.write("<font color='red'>" + arg + "<br>"+"</font>");
+}
+
 
 function deepIndexOf(array,searchString) {
     // Searches first element of each sub array.
@@ -70,7 +74,42 @@ function deepIndexOf(array,searchString) {
     return -1; // search string not found
 }
 
+// Test Code
+var areEqual = function(expected, actual) {
+    return expected === actual;
+}
 
+
+function testPluralizer(func) {
+    var numArguments = 7;
+    var inputOutput = [
+        ['dog','dogs'],
+        ['sheep','sheep'],
+        ['goose','geese'],
+        ['bus','buses'],
+        ['match','matches'],
+        ['box','boxes'],
+        ['tomato','tomatoes'],
+        ['hero','heroes'],
+        ['wolf','wolves'],
+        ['knife','knives'],
+        ['city','cities'],
+        ['baby','babies'],
+    ];
+
+    var results = [];
+    // loop
+    for (let i = 0; i < inputOutput.length; ++i) {
+        results[i] = pluralizer(numArguments,inputOutput[i][0]);
+            show("Input: " + numArguments + " " + inputOutput[i][0]);
+            show("Output: " + results[i]);
+        if( ! areEqual(numArguments + " " + inputOutput[i][1], results[i]) ) {
+            showError("Expected: " + numArguments + " " + inputOutput[i][1]);
+        }
+    }
+}
+
+testPluralizer(pluralizer);
 
 
 
@@ -85,9 +124,6 @@ show(pluralizer(33,'species'));
 show(pluralizer(5,'goose'));
 show(pluralizer(50,'mouse'));
 show(pluralizer(21,'person'));
-// -> remove F/FE, add VES
-show(pluralizer(7,'wolf'));
-show(pluralizer(302,'knife'));
 // -> Ends in S, CH, SH, X, or Z -> add -ES
 show(pluralizer(7,'bus'));
 show(pluralizer(7,'match'));
@@ -99,6 +135,9 @@ show(pluralizer(9,'hero'));
 show(pluralizer(8,'echo'));
 show(pluralizer(5,'tomato'));
 show(pluralizer(4,'potato'));
+// -> remove F/FE, add VES
+show(pluralizer(7,'wolf'));
+show(pluralizer(302,'knife'));
 // -> Ends in CONSONANT + Y -> Remove Y, add -ES
 show(pluralizer(4,'city'));
 show(pluralizer(4,'baby'));
